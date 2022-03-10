47a235a1c8ac54237034f6d6ada3dc88bf88cf74
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-10 14:24:56 +0700

    contoh whitespaces di kotlin

---
+++ src/main/kotlin/c_Whitespaces.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("Hello, world!")
+}
+//Seperti Java, Kotlin juga merupakan bahasa pemrograman yang ditokenisasi. Whitespace tidak
+//signifikan mempengaruhi program dan dengan aman dapat diacuhkan. Kita dapat menggunakan
+//gaya penulisan yang terlihat baik seperti berikut
