f1803d750ebbaad0ffdbb0aa69b9e4ca5a984726
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-10 14:17:59 +0700

    first program

---
+++ src/main/kotlin/a_first_program.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //fun adalah sebuah keyword yang menandakan sebuah fungsi di kotlin
+    // fun main() adalah fungsi main diaplikasi Kotlin dimana sebagai titik awal ketika
+    //program dijalankan.
+    // println() adalah fungsi dasar untuk mencetak string ke layar.
+
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+}
