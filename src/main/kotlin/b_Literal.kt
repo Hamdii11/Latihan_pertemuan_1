b3a4116b2e75cdffa9002985edb728277ffff3b3
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-10 14:27:05 +0700

    contoh whitespaces di kotlin

--- src/main/kotlin/b_Literal.kt
+++ src/main/kotlin/b_Literal.kt
@@ -1,4 +1,10 @@
 fun main() {
+
+    //Kotlin menyediakan beberapa literal untuk tipe dasar (Numbers, Character, Boolean, String).
+    //Literal di Kotlin akan memberikan tipe data secara otomatis setelah program dikompilasi sesuai
+    //dengan nilai yang cocok untuk diberikan pada suatu variabel.
+    //Contoh literal di Kotlin seperti dibawah ini :
+
     var intLiteral = 5
     var doubleLiteral = .02
     var stringLiteral = "Hello"
