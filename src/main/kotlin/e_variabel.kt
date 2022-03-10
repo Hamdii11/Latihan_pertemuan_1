13fd04cc66df25a869d557cb778563a4a3e1c7ef
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-10 14:49:11 +0700

    contoh variabel di kotlin

---
+++ src/main/kotlin/e_variabel.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+}
+//Berdasarkan contoh diatas maka secara otomatis compiler Kotlin akan mengenali bahwa variabel
+//dynamicVar bertipe Int, hal ini identik dengan gaya bahasa pemrograman PHP dan Ruby.
