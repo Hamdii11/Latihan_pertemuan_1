53011484f66f55887bb6afba5b34ee61fab587d6
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-17 09:20:54 +0700

    contoh boolean di kotlin

--- src/main/kotlin/n_Boolean.kt
+++ src/main/kotlin/n_Boolean.kt
@@ -2,7 +2,6 @@ fun main() {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
-
 }
 //Tipe data Boolean memiliki nilai true atau false. Nilai 0 dan 1 tidak berlaku untuk menunjuk
 //nilai pada sebuah variabel bertipe Boolean.
